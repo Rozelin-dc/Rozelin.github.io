e950101034658d999036cf89dcd59df59370a822
Rozelin <ikeda.m.aj@m.titech.ac.jp>
2020-06-27 10:54:37 +0900

    変更

--- works/counter.js
+++ works/counter.js
@@ -2,5 +2,5 @@ let count = 0
 const countUp = () => {
   count++
   const countElement = document.querySelector('#count')
-  countElement.innerText = '回数: ' + count
+  countElement.innerText = 'ｲｴｰｲヽ(^o^)丿: ' + count
 }
